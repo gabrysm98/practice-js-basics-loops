48e938ea8d6c5ce7a613d049f5559618c0a158c8
Gabriel Macierzyński <gabrysm97@gmail.com>
2025-03-30 14:31:20 +0200

    Obliczenie sumy kolejnych liczb zaczynając od 1 a kończąc na "x" z wykorzystaniem pętli "for" a następnie pętli "while"

--- 02/app.js
+++ 02/app.js
@@ -1,2 +1,22 @@
 const x = 5;
 let result = 0;
+
+console.log("wartość zmiennej result przed operacją (pętla for): " + result);
+
+for (let i = 1; i <= x; i++) {
+    result += i;
+}
+
+console.log("wartość zmiennej result po operacji (pętla for): " + result);
+
+
+result = 0;
+let breakPoint = 1;
+
+console.log("wartość zmiennej result przed operacją (pętla while): " + result);
+
+while (breakPoint <= x) {
+    result += breakPoint;
+    breakPoint++;
+}
+console.log("wartość zmiennej result po operacji (pętla while): " + result);
